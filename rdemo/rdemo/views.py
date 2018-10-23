724920aa470e37e3e85f63f9a45f46a79bbd0cfd
chenfh5 <chenfh5@qq.com>
2018-10-23 14:21:41 +0800

    ADD: PersonRESTSet

--- rdemo/rdemo/views.py
+++ rdemo/rdemo/views.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import GenericViewSet
 
 from rdemo.models import Person
+from rdemo.serializers import rDemoSerializer, Validation
 
 logging.basicConfig(level=logging.INFO)
 
@@ -46,7 +47,7 @@ class PersonSet(GenericViewSet):
             obj = obj.filter(name=name)
         if is_not_blank(phone):
             obj = obj.filter(phone=phone)
-        if is_not_blank(age):
+        if is_not_blank(age):  # get para is str
             obj = obj.filter(age__gte=age)  # default is 1
         if is_not_blank(address):
             obj = obj.filter(address__contains=address)
@@ -103,6 +104,95 @@ class PersonSet(GenericViewSet):
         cnt = obj.count()
         obj.delete()
         return Response('destroy %s records' % cnt)
+
+
+class PersonRESTSet(GenericViewSet, Validation):
+    lookup_field = 'instance_id'
+    serializer_class = rDemoSerializer
+
+    def create(self, request):
+        # parameter initial
+        params = self.params_valid(request, serializer=rDemoSerializer)
+        name = params["name"]
+        phone = params["phone"]
+        age = params.get("age", 1)
+        address = params.get("address", "China")
+        logging.info("this is the name=%s, phone=%s, age=%s, address=%s" % (name, phone, age, address))
+
+        # create table
+        obj = Person.objects.create(name=name, phone=phone, age=age, address=address)
+        return Response({"result": True, "data": model_to_dict(obj), "message": "bkdata", "code": "89"})
+
+    def retrieve(self, request, instance_id):
+        # parameter initial
+        params = self.params_valid(request, serializer=rDemoSerializer, skip=True)
+
+        name = params.get("name")
+        phone = params.get("phone")
+        age = params.get("age")
+        address = params.get("address")
+        logging.info("this is the name=%s, phone=%s, age=%s, address=%s" % (name, phone, age, address))
+
+        # table filter
+        obj = Person.objects.all()
+        if is_not_blank(name):
+            obj = obj.filter(name=name)
+        if is_not_blank(phone):
+            obj = obj.filter(phone=phone)
+        if age and age > 0:  # json para is not str
+            obj = obj.filter(age__gte=age)  # default is 1
+        if is_not_blank(address):
+            obj = obj.filter(address__contains=address)
+
+        # table render
+        return Response({"result": True, "data": obj.values(), "message": "bkdata", "code": "88"})
+
+    def update(self, request, instance_id):
+        # parameter initial
+        params = self.params_valid(request, serializer=rDemoSerializer)
+        name = params["name"]
+        phone = params["phone"]
+        age = params.get("age")
+        address = params.get("address")
+        logging.info("this is the name=%s, phone=%s, age=%s, address=%s" % (name, phone, age, address))
+
+        # table filter
+        obj = Person.objects.filter(name=name).filter(phone=phone)
+        if obj.count() > 0:
+            obj = obj.get()  # need to get the instance, otherwise UNIQUE constraint failed caused by save()
+            # table update
+            if age and age > 0:
+                obj.age = age
+            if is_not_blank(address):
+                obj.address = address
+            obj.save()
+            # table render
+            return Response({"result": True, "data": model_to_dict(obj), "message": "bkdata", "code": "89"})
+        else:
+            return Response({"result": False, "data": "record with name=%s, phone=%s not found" % (name, phone), "message": "bkdata", "code": "89"})
+
+    def destroy(self, request, instance_id):
+        # parameter initial
+        params = self.params_valid(request, serializer=rDemoSerializer, skip=True)
+        name = params.get("name")
+        phone = params.get("phone")
+        age = params.get("age")
+        address = params.get("address")
+        logging.info("this is the name=%s, phone=%s, age=%s, address=%s" % (name, phone, age, address))
+
+        # table filter
+        obj = Person.objects.all()
+        if is_not_blank(name):
+            obj = obj.filter(name=name)
+        if is_not_blank(phone):
+            obj = obj.filter(phone=phone)
+        if age and age > 0:
+            obj = obj.filter(age=age)  # default is 1
+        if is_not_blank(address):
+            obj = obj.filter(address=address)
+        cnt = obj.count()
+        obj.delete()
+        return Response({"result": True, "data": "destroy %s records" % cnt, "message": "bkdata", "code": "89"})
 
 
 def is_not_blank(str):
